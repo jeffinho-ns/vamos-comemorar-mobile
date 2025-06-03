ff3c32d17367ca81cf1e419f2875d490d50a9be2
Jefferson Lima <82480661+jeffinho-ns@users.noreply.github.com>
2025-06-03 02:33:32 -0700

    trocando o loading

--- src/app/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/app/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,18 +1,20 @@
 // components/LoadingSpinner.tsx
 import React from 'react';
+import Image from 'next/image'; // Importe o componente Image do Next.js
 
 const LoadingSpinner: React.FC = () => {
   return (
+    // Este é o container central que alinha o conteúdo
     <div className="flex flex-col items-center justify-center">
-      {/* Container para as barras de onda sonora */}
-      <div className="flex items-end h-24 w-24 justify-between space-x-2 border border-red-500"> {/* Borda vermelha para depuração */}
-        {/* Barra 1 - Cor mais clara */}
-        <div className="w-4 h-10 bg-lime-400 rounded-full animate-wave-pulse-1"></div> {/* Adicionei h-10 */}
-        {/* Barra 2 - Cor mais clara */}
-        <div className="w-4 h-10 bg-cyan-400 rounded-full animate-wave-pulse-2"></div> {/* Adicionei h-10 */}
-        {/* Barra 3 - Cor mais clara */}
-        <div className="w-4 h-10 bg-amber-400 rounded-full animate-wave-pulse-3"></div> {/* Adicionei h-10 */}
-      </div>
+      {/* Imagem do GIF */}
+      <Image
+        src="/intro/loading.mp4" // <--- Altere para o caminho do seu GIF na pasta `public`
+        alt="Carregando..."
+        width={150} // Ajuste a largura conforme o tamanho do seu GIF
+        height={150} // Ajuste a altura conforme o tamanho do seu GIF
+        unoptimized // Essencial para GIFs para evitar otimizações que podem quebrar a animação
+        className="mb-4" // Margem inferior para separar do texto
+      />
       <p className="mt-4 text-lg text-white">Carregando eventos...</p>
     </div>
   );
--- src/app/tailwind.config.js
+++ src/app/tailwind.config.js
@@ -8,18 +8,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      keyframes: {
-        'wave-pulse': { // Novo keyframe para a animação de onda
-          '0%, 100%': { height: '25%' }, // Inicia e termina baixo
-          '50%': { height: '100%' },   // Atinge o pico no meio
-        },
-      },
-      animation: {
-        // Novas animações para as barras, com atrasos diferentes
-        'wave-pulse-1': 'wave-pulse 1.2s ease-in-out infinite',
-        'wave-pulse-2': 'wave-pulse 1.2s ease-in-out infinite 0.2s', // Atraso de 0.2s
-        'wave-pulse-3': 'wave-pulse 1.2s ease-in-out infinite 0.4s', // Atraso de 0.4s
-      },
+
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
